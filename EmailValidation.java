03feaed03db176f6e207f769fe8b83ab4ca8e04c
KratiGupta <kratigupta8545@gmail.com>
2021-08-13 00:52:12 +0530

    Added to validate the first part only

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,9 +1,25 @@
 package regexproblems;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Master Branch Of Email Validation Problem Of Regex");
+		
+		      Scanner sc = new Scanner(System.in);
+		      System.out.println("Enter your Email: ");
+		      String email = sc.next();
+		      String regex = "^[^.].+$";
+		      Pattern pattern = Pattern.compile(regex);
+		      //Matching the given phone number with regular expression
+		      
+		      Matcher matcher = pattern.matcher(email);
+		      
+		         System.out.println(email +" : "+ matcher.matches());
+		     
+		   }
 	}
 
-}
+
