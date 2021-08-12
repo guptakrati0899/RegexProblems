99da2a0aab2c884b4c5b23875173b27ed9bedf04
KratiGupta <kratigupta8545@gmail.com>
2021-08-13 00:10:02 +0530

    Added Welcome Message Successfully

---
+++ EmailValidation.java
@@ -0,0 +1,9 @@
+package regexproblems;
+
+public class EmailValidation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Master Branch Of Email Validation Problem Of Regex");
+	}
+
+}
